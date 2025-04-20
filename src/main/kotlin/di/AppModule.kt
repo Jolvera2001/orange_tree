6eafb87ebbd93838bbb5aea3289986699747de63
Jolvera2001 <jolvera6@stedwards.edu>
2025-04-20 00:25:40 -0500

    put service in koin module

--- src/main/kotlin/di/AppModule.kt
+++ src/main/kotlin/di/AppModule.kt
@@ -1,9 +1,12 @@
 package dev.jolvera.di
 
+import dev.jolvera.services.BlogService
 import dev.jolvera.services.GithubClient
 import org.koin.dsl.module
 
 val appModule = module {
     single { MongoData() }
     single { GithubClient() }
+
+    factory { BlogService(get()) }
 }
