dc8ea21d7109d438b91f1e746b83e4b05030eb92
Jolvera2001 <jolvera6@stedwards.edu>
2025-04-20 00:02:14 -0500

    updated blog model

--- src/main/kotlin/models/Blog.kt
+++ src/main/kotlin/models/Blog.kt
@@ -1,13 +1,16 @@
 package dev.jolvera.models
 
 import kotlinx.datetime.LocalDateTime
+import org.bson.types.ObjectId
 import java.util.UUID
 
 data class Blog(
+    val _id: ObjectId? = null,
+    val slug: String,
     val title: String,
     val subtitle: String,
     val author: String,
-    val body: String,
+    val content: String,
     val updatedAt: LocalDateTime,
     val createdAt: LocalDateTime,
 )
